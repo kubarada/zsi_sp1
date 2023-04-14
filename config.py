cb68f7ce109514a5e4be171f763098e9ec19c716
Jakub Rada <127985335+kubarada@users.noreply.github.com>
2023-04-14 10:09:01 +0200

    progres, implementing first FIR

--- config.py
+++ config.py
@@ -1,14 +1,36 @@
 import matplotlib.pyplot as plt
+from scipy.signal import firwin, lfilter
+import numpy as np
 
 
 # prostě plotovací fce, abych nemusel milionkrát psát plt., plt.,.....
-def plot(x, y, legend = ['x', 'y'], lim = ['', '', '', '']):
+def plot(x, y, legend = ['x', 'y'], lim = ['', '', '', ''], title = 'Doplnit title!'):
 
     plt.figure()
     plt.plot(x, y)
     plt.xlabel(legend[0])
+    plt.title(title)
+    plt.grid()
     plt.ylabel(legend[1])
     if lim[0] != '':
-        plt.xlim(lim[0], lim[1])
-        plt.ylim(lim[2], lim[3])
+        plt.xlim(float(lim[0]), float(lim[1]))
+        plt.ylim(float(lim[2]), float(lim[3]))
+    plt.show()
+
+def lowpass(data, sample_rate, cutoff_freq, filter_order):
+
+    # Generate the filter coefficients using the firwin function
+    nyquist_rate = sample_rate / 2
+    filter_coeffs = firwin(filter_order, cutoff_freq/nyquist_rate)
+
+
+    # Apply the filter to the test signal
+    filtered_signal = lfilter(filter_coeffs, 1, data)
+
+    # Plot the original and filtered signals
+    plt.figure(figsize=(15, 5))
+    plt.specgram(filtered_signal, Fs=sample_rate, vmin=-20, vmax=50)
+    plt.title('Spektogram')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [s]')
     plt.show()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import config as cnf
 
-INPUT_FILE = 'veta.wav'
+INPUT_FILE = 'C:/Users/qradj/OneDrive - AIMTEC a. s/Desktop/zsi_sp1./veta.wav'
 
 rate, data = sc.wavfile.read(INPUT_FILE)
 
@@ -15,11 +15,20 @@ hop_length = win // 2
 spec = np.abs(np.fft.rfft(data, n=win, axis=0))
 freqs = np.fft.rfftfreq(win, d=1/rate)
 t = np.arange(0, len(data)) / rate
+print('Lenght of audiofile: ' + str(t[-1]) + ' s')
 
 # ploting audio wave
-cnf.plot(t, data)
+cnf.plot(t, data, ['Time [s]', 'Signal value'], lim = [0, t[-1], -31000, 25000],  title = 'Časový vývoj signálu')
 
 # ploting the spectrum of audio
-cnf.plot(freqs, spec, ["Frequency (Hz)", "Magnitude"])
+cnf.plot(freqs, spec, ["Frequency [Hz]", "Magnitude [dB]"], lim = [0, 22000, 0, 460000], title = 'Spektrum signálu')
+
+plt.figure(figsize=(15, 5))
+plt.specgram(data, Fs=rate, vmin=-20, vmax=50)
+plt.title('Spektogram')
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [s]')
+plt.show()
 
 
+cnf.lowpass(data, sample_rate = rate, cutoff_freq = 5600, filter_order=170) 
