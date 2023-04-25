c66a137b80c944741e4753ec3858af504216de91
Jakub Rada <127985335+kubarada@users.noreply.github.com>
2023-04-25 14:47:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ def lowpass(data, sample_rate, cutoff_freq, filter_order, title):
     # Generate the filter coefficients using the firwin function
     nyquist_rate = sample_rate / 2
     filter_coeffs = firwin(filter_order, cutoff_freq/nyquist_rate)
-    file_name = 'plots/lp_' + str(filter_order) + '.eps'
+    file_name = 'lp_' + str(filter_order) + '.eps'
 
     # Apply the filter to the test signal
     filtered_signal = lfilter(filter_coeffs, 1, data)
@@ -58,7 +58,7 @@ def bandpass(data, sample_rate, cutoff_freq1, cutoff_freq2, filter_order, title)
     filtered_signal2 = filtered_signal2[n_transient:]
     filtered_signal = filtered_signal1 + filtered_signal2
 
-    file_name = 'plots/bp_' + str(filter_order) + '.eps'
+    file_name = 'bp_' + str(filter_order) + '.eps'
     # Plot the original and filtered signals
     plt.figure(figsize=(15, 5))
     plt.specgram(filtered_signal, Fs=sample_rate, vmin=-20, vmax=50)
@@ -76,7 +76,7 @@ def irbutter(data, sample_rate, cutoff_freq1, filter_order, title):
 
     # Apply filter to signal
     filtered_signal = filtfilt(b, a, data)
-    file_name = 'plots/butt_' + str(filter_order) + '.eps'
+    file_name = 'butt_' + str(filter_order) + '.eps'
     # Plot the original and filtered signals
     plt.figure(figsize=(15, 5))
     plt.specgram(filtered_signal, Fs=sample_rate, vmin=-20, vmax=50)
@@ -86,3 +86,35 @@ def irbutter(data, sample_rate, cutoff_freq1, filter_order, title):
     plt.savefig(file_name, format='eps', dpi = 50)
     plt.show()
 
+
+def oversample(data, sample_rate, freq):
+    t = np.arange(len(data)) / sample_rate
+    t_new = np.arange(0, t[-1], 1/8000)
+    y_new = np.interp(t_new, t, data)
+    nyquist = sample_rate / 2.0
+    cutoff = 8000 / nyquist
+    b, a = butter(8, cutoff)
+    y_filtered = filtfilt(b, a, y_new)
+
+    return y_filtered, t_new
+
+def signal_segment(data, N):
+    m = 2 ** N
+    segments = []
+    for i in range(0, len(data), m):
+        segment = data[i:i+m]
+        if len(segment) == m:
+            segments.append(segment)
+
+    segments = np.array(segments)
+
+    return segments
+
+def fft(x):
+    N = len(x)
+    if N <= 1:
+        return x
+    even = fft(x[0::2])
+    odd = fft(x[1::2])
+    factor = np.exp(-2j * np.pi * np.arange(N) / N)
+    return np.concatenate([even + factor[:N//2] * odd, even + factor[N//2:] * odd])
