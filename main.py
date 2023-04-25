a57cd027b1adabf93175b70948196c49b12016d7
Jakub Rada <127985335+kubarada@users.noreply.github.com>
2023-04-25 14:46:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import scipy.io as sc
 import matplotlib.pyplot as plt
 import numpy as np
-from pydub import AudioSegment
-from pydub.playback import play
-
-
 import config as cnf
 
 INPUT_FILE = 'veta.wav'
 
-# import required module
-from playsound import playsound
-
-# for playing note.wav file
 rate, data = sc.wavfile.read(INPUT_FILE)
 
 # Compute the power spectrum of the audio signal
@@ -34,7 +26,7 @@ plt.title('Spektrogram')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [s]')
 plt.show()
-
+"""
 # fir lowpass
 cnf.lowpass(data, sample_rate = rate, cutoff_freq = 5700, filter_order=170, title = 'Spectrogram - filter order = 170')
 cnf.lowpass(data, sample_rate = rate, cutoff_freq = 5700, filter_order=190,title = 'Spectrogram - filter order = 190')
@@ -50,11 +42,17 @@ cnf.bandpass(data, sample_rate = rate, cutoff_freq1= [1,5800], cutoff_freq2 = [6
 cnf.bandpass(data, sample_rate = rate, cutoff_freq1= [1,5800], cutoff_freq2 = [6200, 19000],filter_order = 300,  title = 'Spectrogram - filter order = 300')
 
 # iir lowpass butterwoth
-cnf.irbutter(data, sample_rate = rate, cutoff_freq1= 5700,filter_order = 5,  title = 'Spectrogram - filter order = 5')
-cnf.irbutter(data, sample_rate = rate, cutoff_freq1= 5700,filter_order = 10,  title = 'Spectrogram - filter order = 10')
-cnf.irbutter(data, sample_rate = rate, cutoff_freq1= 5700,filter_order = 15,  title = 'Spectrogram - filter order = 15')
-cnf.irbutter(data, sample_rate = rate, cutoff_freq1= 5700,filter_order = 20,  title = 'Spectrogram - filter order = 20')
-cnf.irbutter(data, sample_rate = rate, cutoff_freq1= 5700,filter_order = 25,  title = 'Spectrogram - filter order = 25')
+cnf.irbutter(data, sample_rate = rate, cutoff_freq1 = 5700,filter_order = 5,  title = 'Spectrogram - filter order = 5')
+cnf.irbutter(data, sample_rate = rate, cutoff_freq1 = 5700,filter_order = 10,  title = 'Spectrogram - filter order = 10')
+cnf.irbutter(data, sample_rate = rate, cutoff_freq1 = 5700,filter_order = 15,  title = 'Spectrogram - filter order = 15')
+cnf.irbutter(data, sample_rate = rate, cutoff_freq1 = 5700,filter_order = 20,  title = 'Spectrogram - filter order = 20')
+cnf.irbutter(data, sample_rate = rate, cutoff_freq1 = 5700,filter_order = 25,  title = 'Spectrogram - filter order = 25')
+
+"""
+# převzorkování
+
+data_re, t_new =  cnf.oversample(data, rate, 8000)
+cnf.plot(t_new, data_re, ['Time [s]', 'Signal value'], lim = [0, t[-1], -31000, 25000],  title = 'Časový vývoj signálu')
 
 
 
